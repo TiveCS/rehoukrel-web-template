1c739112643dac3d941704eff9fb7248fc126f9b
fathan <fathan.hanif.k@len.co.id>
2025-12-17 14:23:07 +0700

    chore: add more dice ui components

--- apps/web/src/routes/playground/index.tsx
+++ apps/web/src/routes/playground/index.tsx
@@ -1,23 +1,11 @@
+import { AppFileUpload } from "@/components/blocks/app-file-upload";
 import { createFileRoute } from "@tanstack/react-router";
-import { useState } from "react";
-import { MultiSelect } from "@/components/blocks/multi-select";
-import { FileUpload } from "@/components/ui/file-upload";
-import { AppFileUpload } from "@/components/blocks/AppFileUpload";
 
 export const Route = createFileRoute("/playground/")({
   component: PlaygroundPage,
 });
 
 function PlaygroundPage() {
-  const [value, setValue] = useState<string[]>([]);
-  const tricks = [
-    { value: "backflip", label: "Backflip" },
-    { value: "frontflip", label: "Frontflip" },
-    { value: "sideflip", label: "Sideflip" },
-    { value: "360", label: "360" },
-    { value: "720", label: "720" },
-  ];
-
   return (
     <div className="p-8">
       <AppFileUpload />
